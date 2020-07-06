aec1e75968de3bb66285a4b9d9a61f83c737ed95
Kaizad <kaizad.patel@pnnl.gov>
2020-07-06 14:25:29 -0700

    create flux script. calculate flux summaries

---
+++ code/3-fluxes.R
@@ -0,0 +1,49 @@
+## SPATIAL ACCESS
+## KAIZAD F. PATEL
+## 6-JULY-2020
+
+## GREENHOUSE GAS FLUXES
+
+# the data are processed and outliers have been removed
+# data are available here as flux values per time point
+
+source("code/0-packages.R")
+
+
+# load files --------------------------------------------------------------
+corekey = read.csv("data/processed/corekey.csv")
+fluxes_input = read.csv("data/GHG_Flux_exclOutliers.csv")
+
+## clean the flux data
+
+flux = 
+  fluxes_input %>% 
+  select(ID, DATETIME, CORE, Homogenization, elapsed_minutes, `ElapsedMin.BIN.`,
+         `CO2_flux_umol.g.s`, `CH4_flux_umol.g.s`) %>% 
+  rename(CO2_umol_g_s = `CO2_flux_umol.g.s`,
+         CH4_umol_g_s = `CH4_flux_umol.g.s`,
+         elapsed_min_bin = `ElapsedMin.BIN.`) %>% 
+  left_join(corekey, by = c("CORE"="Core")) %>% 
+  mutate(Homogenization = recode(Homogenization, " Intact" = "Intact"))
+
+
+# summaries ---------------------------------------------------------------
+
+## calculate summaries
+
+flux_summary = 
+  flux %>% 
+  group_by(ID, CORE, Homogenization) %>% 
+  summarise(mean_CO2_nmol_g_s = mean(CO2_umol_g_s*1000),
+            cum_CO2_nmol_g_s = sum(CO2_umol_g_s*1000)) %>% 
+  left_join(corekey, by = c("CORE"="Core"))
+
+
+
+# output ------------------------------------------------------------------
+
+write.csv(flux, "data/processed/flux.csv", row.names = F)
+write.csv(flux_summary, "data/processed/flux_summary.csv", row.names = F)
+
+
+
