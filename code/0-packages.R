77570712cf49662d844ee4a95d26f8f8e8d957e2
kaizadp <50244730+kaizadp@users.noreply.github.com>
2020-07-24 17:46:13 -0700

    adjust theme_kp. make lines thinner. reduce axis text size.

--- code/0-packages.R
+++ code/0-packages.R
@@ -22,16 +22,16 @@ theme_kp <- function() {  # this for all the elements common across plots
           legend.text = element_text(size = 12),
           legend.key.size = unit(1.5, 'lines'),
           legend.background = element_rect(colour = NA),
-          panel.border = element_rect(color="black",size=1.5, fill = NA),
+          panel.border = element_rect(color="black",size=1, fill = NA),
           
-          plot.title = element_text(hjust = 0.05, size = 14),
-          axis.text = element_text(size = 14, color = "black"),
+          plot.title = element_text(hjust = 0, size = 14),
+          axis.text = element_text(size = 12, color = "black"),
           axis.title = element_text(size = 14, face = "bold", color = "black"),
           
           # formatting for facets
           panel.background = element_blank(),
           strip.background = element_rect(colour="white", fill="white"), #facet formatting
-          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
+          panel.spacing.x = unit(1, "lines"), #facet spacing for x axis
           panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
           strip.text.x = element_text(size=12, face="bold"), #facet labels
           strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
